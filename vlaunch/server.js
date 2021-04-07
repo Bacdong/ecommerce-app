ecc5f5d50f57a0e1fd875faa5fcdb7c78d3e05c2
Bacdong <duongdong2203@gmail.com>
2021-04-07 13:12:24 +0700

    Update deployment script

--- vlaunch/server.js
+++ vlaunch/server.js
@@ -1,15 +1,24 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+//Install express server
 const path = require('path');
 
-const app = express();
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/LavisDecor-Web/vlaunch'));
+app.use(express.static(__dirname + '/dist/ecommerce-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/LavisDecor-Web/vlaunch/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/LavisDecor-Web/vlaunch/index.html'));
+res.sendFile('index.html', {root: 'dist/ecommerce-app/'})
 });
 
 // Start the app by listening on the default Heroku port
