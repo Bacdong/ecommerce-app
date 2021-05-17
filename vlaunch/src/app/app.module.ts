a0f78b24a45972c1a1b09816eac2959c90309ba7
Ninh Ngoc Hieu <30309069+ninhngochieu@users.noreply.github.com>
2021-05-17 16:16:53 +0700

    Revert "Revert "Merge branch 'Thêm-sidebar' into dev""
    This reverts commit 9318f67e48122aa34350bc9287f3209f7b5b136c.

--- vlaunch/src/app/app.module.ts
+++ vlaunch/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { AuthModule } from './modules/auth/auth.module';
 import { MaterialModule } from './modules/vlaunch/material.module';
 import { VlaunchModule } from './modules/vlaunch/vlaunch.module';
-import {StripeModule} from 'stripe-angular';
 import {NgxStripeModule} from 'ngx-stripe';
 
 
@@ -32,7 +31,8 @@ import {NgxStripeModule} from 'ngx-stripe';
     MaterialModule,
   NgxStripeModule.forRoot('pk_test_51Hds9CDa9Wt0jMavaIgUpFqwPCcVq50HmzBk5j2maKvaiGnoaZXNxee668jIGqdEmSETbBgokFc84Sk4fWYvLZqQ00PQk8i2eK')
   ],
-  exports: [],
+  exports: [
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
